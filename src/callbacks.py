7f9de2a4c91763aa3e3f085280081df829a34d34
oq-Yuki-op <o.yukinko0715@gmail.com>
2021-10-20 07:11:16 +0000

    コメント追加

--- src/callbacks.py
+++ src/callbacks.py
@@ -3,11 +3,27 @@ import tensorflow as tf
 
 
 def cb_tensorboard(log_dir):
+    """TensorBoardのCallback
+
+    Args:
+        log_dir ([src]): ログを保存するパス
+
+    Returns:
+        [tf.keras.callbacks]: TensorBoardのCallback
+    """
 
     return tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1, update_freq=3)
 
 
 def cb_checkpoint(checkpoint_dir):
+    """Check pointのCallback
+
+    Args:
+        checkpoint_dir (src): check pointを保存するパス
+
+    Returns:
+        [tf.keras.callbacks]: check pointのCallback
+    """
     checkpoint_path = f"{checkpoint_dir}" + "cp-{epoch:03d}_{loss:.4f}.ckpt"
 
     return tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
